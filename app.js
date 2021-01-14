8b54b5c5fc1861a216ee318be9ebc0052f2fd7ac
Yousef-Medhat56 <yousif.medhat562004@gmail.com>
2021-01-14 12:46:57 +0200

    The project code

---
+++ app.js
@@ -0,0 +1,33 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const navBar = document.querySelector('.nav-bar-list');
+    const navLink = document.querySelectorAll('.nav-bar-list li')
+
+    burger.addEventListener('click' , () => {
+        navBar.classList.toggle("nav-active");
+    
+
+        navLink.forEach((link,index) => {
+           if(link.style.animation){
+            link.style.animation = "";
+           }
+           else{
+            link.style.animation = `navBarFade 0.3s ease forwards ${index / 7 + 0.5}s`
+           }
+        }); 
+
+        burger.classList.toggle('toggle');
+    });
+}
+
+navSlide();
+
+
+
+  
+
+
+
+
+
+
