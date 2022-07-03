ea7cb645213f381aea1cd5ca94c89830564e300a
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2022-07-03 22:04:07 +0200

    set the nav bar position to fixed

--- app.js
+++ app.js
@@ -3,31 +3,20 @@ const navSlide = () => {
     const navBar = document.querySelector('.nav-bar-list');
     const navLink = document.querySelectorAll('.nav-bar-list li')
 
-    burger.addEventListener('click' , () => {
+    burger.addEventListener('click', () => {
         navBar.classList.toggle("nav-active");
-    
 
-        navLink.forEach((link,index) => {
-           if(link.style.animation){
-            link.style.animation = "";
-           }
-           else{
-            link.style.animation = `navBarFade 0.3s ease forwards ${index / 7 + 0.5}s`
-           }
-        }); 
+
+        navLink.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = "";
+            } else {
+                link.style.animation = `navBarFade 0.2s ease forwards ${index / 7 + 0.5}s`
+            }
+        });
 
         burger.classList.toggle('toggle');
     });
 }
 
 navSlide();
-
-
-
-  
-
-
-
-
-
-
